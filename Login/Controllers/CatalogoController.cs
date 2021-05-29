23da67fe22c73874cb6cfe51b11a20e12836d668
Erik Espinoza <erik_espinoza_mendoza@hotmail.com>
2021-05-28 22:05:41 -0500

    Login terminado papu

---
+++ Login/Controllers/CatalogoController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Login.Models;
+using Login.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace Login.Controllers{
+
+    public class CatalogoController : Controller {
+public async Task<IActionResult> Add(int? id)
+        {
+        private readonly ILogger<CatalogoController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+
+        public CatalogoController(ILogger<CatalogoController> logger,
+            ApplicationDbContext context,
+            UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _logger = logger;
+            _userManager = userManager;
+        }
+            var userID = _userManager.GetUserName(User);
+            if(userID == null){
+                ViewData["Message"] = "Por favor debe loguearse antes de agregar un producto";
+                List<Producto> productos = new List<Producto>();
+                return  View("Index",productos);
+            }else{
+                var producto = await _context.DataProductos.FindAsync(id);
+                Proforma proforma = new Proforma();
+                proforma.Producto = producto;
+                proforma.Price = producto.Price;
+                proforma.Quantity = 1;
+                proforma.UserID = userID;
+                _context.Add(proforma);
+                await _context.SaveChangesAsync();
+                return  RedirectToAction(nameof(Index));
+            }
+        }
+        }
+        }
